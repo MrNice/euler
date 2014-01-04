015125aacaafd74f28d6bc429a676c69b9a19e16
Nicholas <nicholas@niceguys.com>
2014-01-03 18:50:42 -0800

    Finished problem 2, Gen'd fibs w/ for loop

--- js/src/euler.js
+++ js/src/euler.js
@@ -1,6 +1,24 @@
+//Solution 2
+//Even Fibs added up
+var evenFibs = function(number) {
+  var fibs = [];
+
+  //Traditional Fibonacci Sequence Generator
+  for(var i = 1, memo = 0; i < number;){
+    fibs.push(i);
+    i = memo + i;
+    memo = i - memo;
+  }
+
+  return _(fibs).chain()
+          .filter(function(num) { return (num % 2 === 0); })
+          .reduce(function(memo, num) { return memo + num; })
+          .value();
+            
+}
+
 //Solution 1
 //Multiples of 3 and 5
-
 var multiples = function(number) {
   var array = _.range(number);
   return _(array).chain()
